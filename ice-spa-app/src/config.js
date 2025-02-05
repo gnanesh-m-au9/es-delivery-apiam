6e94d3b3ebe946d37358eecc3fff43b5ede0ad35
Sara Kazemi <sara.kazemi@okta.com>
2025-02-05 23:54:11 +0000

    Update line numbers

--- ice-spa-app/src/config.js
+++ ice-spa-app/src/config.js
@@ -2,7 +2,7 @@
 export default {
   oidc: {
     clientId: "XXXXXXXXXXXXXXXXXX", // 👈 update with your Client ID
-    issuer: "https://{domain}.okta.com", // 👈 update 
+    issuer: "https://{domain}.okta.com", // 👈 update
     redirectUri: window.location.origin + "/login/callback",
     // authorizationEndpoint: "https://{domain}.okta.com/oauth2/axxxxxxxxxxxx/v1/authorize",
     scopes: ["openid", "profile", "email"],
